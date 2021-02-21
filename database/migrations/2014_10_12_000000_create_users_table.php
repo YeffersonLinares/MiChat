aa4abfa1490cdf3d7c98733f4b8e09e7ef6403c9
Yefferson_Linares <ylinares@unbosque.edu.co>
2021-02-20 20:15:36 -0500

    base de datos inicial

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable($value = true);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable($value = true);
+            $table->string('password')->nullable($value = true);
             $table->rememberToken();
             $table->timestamps();
         });
